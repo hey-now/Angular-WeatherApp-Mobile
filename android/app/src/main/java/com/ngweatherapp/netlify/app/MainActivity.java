393dac1da1d38c407aec1c921b1ad6deb2c2fb6b
hey-now <rubinand22@gmail.com>
2024-11-10 13:33:49 -0800

    working in android studio

---
+++ android/app/src/main/java/com/ngweatherapp/netlify/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ngweatherapp.netlify.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ngweatherapp.netlify.app',
+  appName: 'Angular Weather App',
+  webDir: 'dist/ng-weather-app'
+};
+
+export default config;
